a8465672c00527dc3e900859c3b2068a6e887696
Manish Nemade <166635137+Manishnemade12@users.noreply.github.com>
2025-09-23 07:53:23 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,3 @@
-// import type {NextConfig} from 'next';
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'placehold.co',
-//         port: '',
-//         pathname: '/**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'images.unsplash.com',
-//         port: '',
-//         pathname: '/**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'picsum.photos',
-//         port: '',
-//         pathname: '/**',
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -67,16 +30,53 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-
-
- webpack: (config, { dev }) => {
-    // ❌ disable eval in production
-    if (!dev) {
-      config.devtool = false;
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
+
+
+// import type {NextConfig} from 'next';
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'placehold.co',
+//         port: '',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'picsum.photos',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+
+
+//  webpack: (config, { dev }) => {
+//     // ❌ disable eval in production
+//     if (!dev) {
+//       config.devtool = false;
+//     }
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
 
